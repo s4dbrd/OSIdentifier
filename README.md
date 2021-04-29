568592ac30f8b6d04fb2c78d8ea3f09c656df4a3
Adrian Diaz <adriandiazag@gmail.com>
2021-04-29 14:40:07 +0200

    Terminado

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # osidentifier
+
+osidentifier es una herramienta simple creada en bash que permite identificar el sistema operativo de una máquina a través del TTL.
+
+## Instalación
+Clonamos el repositorio en el directorio que deseemos:<br/>
+>git clone https://github.com/adiaguilar/osidentifier.git<br/><br/>
+>cd osidentifier<br/><br/>
+>chmod +x osidentifier.sh<br/>
+
+Para mayor facilidad, podemos poner esta herramienta en el path de configuración de nuestra shell y así poder ejecutarla en cualquier directorio sin necesidad de estar en la ruta del script, se recomienda introducir el script en *$HOME/.local/bin* para mayor comodidad:<br/>
+>mv osidentifier.sh ~/.local/bin<br/><br/>
+>sudo nano ~/.bashrc<br/><br/>
+>Añadir al PATH=/home/$USER/.local/bin<br/><br/>
+>source ~/.bashrc<br/>
+
+## Uso de la herramienta
+
+osidentifier cuenta con un parámetro de ayuda, también tiene otros dos parámetros donde se puede introducir una dirección IP o anexar un fichero con múltiples direcciones IP.
+```
+-h:	Muestra el panel de ayuda
+-i:	Sirve para introducir la dirección IP (Uso: ./osidentifier -i 8.8.8.8)
+-f: Introducir un fichero con direcciones IP (Uso: ./osidentifier -f ips.txt)
+```
--- osidentifier.sh
+++ osidentifier.sh
@@ -10,36 +10,82 @@ purpleColour="\e[0;35m\033[1m"
 turquoiseColour="\e[0;36m\033[1m"
 grayColour="\e[0;37m\033[1m"
 
-#Función muestra menú de ayuda para el usuario
+trap ctrl_c INT
+
+function ctrl_c(){
+	echo -e "\n${yellowColour}[*]${endColour}${grayColour} Saliendo... ${endColour}${yellowColour}[*]${endColour}"
+	rm /tmp/tmp.* 2>/dev/null
+	exit 0
+}
+
 function MenuAyuda(){
     echo -e "\n${greenColour}Ayuda de osidentifier.sh${endColour}"
     echo -e "\n\t${yellowColour}-i${endColour}${turquoiseColour} Introducir dirección IP${endColour}"
     echo -e "\t\t${greenColour}Ejemplo:${endColour}${blueColour}./osidentifier.sh -i 172.22.222.1${endColour}"
-    echo -e "\n\t${yellowColour}-f${endColour}${turquoiseColour} Introducir fichero con direcciones${endColour}"
+    echo -e "\n\t${yellowColour}-f${endColour}${turquoiseColour} Introducir fichero con direcciones IP${endColour}"
     echo -e "\t\t${greenColour}Ejemplo:${endColour}${blueColour}./osidentifier.sh -f ips.txt${endColour}"
     exit 0
 }
 
 function Readip(){
     clear
-    ttl=$(ping -c 1 $2 | grep ttl | sed 's/.*ttl=\([[:digit:]]*\).*/\1/')
-    echo -e "\n${yellowColour}TTL->${endColour}${turquoiseColour}${ttl}"
-    if [ '0' -g "$ttl" -a "$ttl" -le '64' ]; then
-        echo -e "\n${greenColour}Linux"
-    elif [ '64' -g "$ttl" -a "$ttl" -le '128' ]; then
-        echo -e "\n${greenColour}Windows"
-    elif [ '128' -g "$ttl" -a "$ttl" -le '255' ]; then
-        echo -e "\n${greenColour}Solaris"
+    ttl=$(ping -c 1 -W 0.5 $ip | grep ttl | sed 's/.*ttl=\([[:digit:]]*\).*/\1/')
+    if [[ "$ttl" ]]; then
+    echo -e "\n${yellowColour}TTL-> ${endColour}${turquoiseColour}${ttl}"
+    if [[ 0 -lt $ttl ]] && [[ $ttl -le 64 ]]; then
+        echo -e "${greenColour}Linux"
+    elif [[ 64 -lt $ttl ]] && [[ $ttl -le 128 ]]; then
+        echo -e "${greenColour}Windows"
+    elif [[ 128 -lt $ttl ]] && [[ $ttl -le 255 ]]; then
+        echo -e "${greenColour}Solaris"
+    fi
+    else
+    echo -e "\n${purpleColour}IP $cont:${endColour} ${turquoiseColour}$ip"
+    echo -e "\n\t${redColour}[!] El ping ha fallado, puede ser que el Firewall esté bloqueando las trazas ping [!]${endColour}"
     fi
 }
 
-while [[ "$#" -gt 0 ]]; do
-    case $1 in
-        -h|--help)
-        MenuAyuda
-        ;;
-        -i|--ip)
-        Readip
-        ;;
+function ReadFile(){
+    clear
+    RegularExpression='(([0-9]|[0-9]{2}|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[0-9]{2}|1[0-9]{2}|2[0-4][0-9]|25[0-5])'
+    tfile=$(mktemp)
+    ip=$(egrep -o "$RegularExpression" $file >> $tfile)
+    declare -i cont=1
+    while read line; do
+        ttl=$(ping -c 1 -W 0.5 $line | grep ttl | sed 's/.*ttl=\([[:digit:]]*\).*/\1/')
+            echo -e "\n${purpleColour}IP $cont:${endColour} ${turquoiseColour}$line"
+            let cont+=1
+            if [[ "$ttl" ]]; then
+            echo -e "\n\t${yellowColour}TTL-> ${endColour}${turquoiseColour}${ttl}"
+            if [ 0 -lt $ttl ] && [ $ttl -le 64 ]; then
+                echo -e "\t${greenColour}Linux"
+            elif [ 64 -lt $ttl ] && [ $ttl -le 128 ]; then
+                echo -e "\t${greenColour}Windows"
+            elif [ 128 -lt $ttl ] && [ $ttl -le 255 ]; then
+                echo -e "\t${greenColour}Solaris"
+            fi
+            else
+            echo -e "\n\t${redColour}[!] El ping ha fallado, puede ser que el Firewall esté bloqueando las trazas ping [!]${endColour}"
+            fi
+    done < $tfile
+    rm $tfile
+}
+
+declare -i cont_par=0; while getopts ":h:i:f:" arg; do
+    case $arg in
+        h) MenuAyuda;;
+        i) ip=$OPTARG; let cont_par+=1;;
+        f) file=$OPTARG; let cont_par+=2;;
     esac
 done
+
+if [ $cont_par -eq 0 ]; then
+    MenuAyuda
+elif [ $cont_par -eq 1 ]; then
+    Readip
+elif [ $cont_par -eq 2 ]; then
+    ReadFile
+elif [ $cont_par -eq 3 ]; then
+    Readip
+    ReadFile
+fi
